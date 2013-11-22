633f39b9946a0093d33eab4b9b50df6d944c6279
anitan11 <anitan11@student.uia.no>
2013-11-22 10:58:21 +0100

    Missing subject as link

---
+++ lesson3-is206/main.py
@@ -0,0 +1,80 @@
+import os
+import webapp2
+import jinja2
+
+from google.appengine.ext import db
+
+web_dir = os.path.join(os.path.dirname(__file__), 'web')
+JINJA_ENVIRONMENT = jinja2.Environment(loader = jinja2.FileSystemLoader(web_dir), autoescape=True)
+
+def render_str(template, **params):
+    t = JINJA_ENVIRONMENT.get_template(template)
+    return t.render(params)
+
+class MainHandler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        return render_str(template, **params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+def render_post(response, post):
+    response.out.write('<b>' + post.subject + '</b><br>')
+    response.out.write(post.content)
+
+def blog_key(name = 'default'):
+    return db.Key.from_path('blogs', name)
+
+class PostDB(db.Model):
+	subject = db.StringProperty(required = True)
+	content = db.TextProperty(required = True)
+	created = db.DateTimeProperty(auto_now_add = True)
+	last_modified = db.DateTimeProperty(auto_now = True)
+
+	def render(self):
+		self._render_text = self.content.replace('\n', '<br>')
+		return render_str("post.html", p = self)
+
+class Blog(MainHandler):
+    def get(self):
+        posts = db.GqlQuery("select * from PostDB order by created desc limit 10")
+        self.render('front.html', posts = posts)
+
+class Post(MainHandler):
+    def get(self, post_id):
+        key = db.Key.from_path('Post', int(post_id), parent=blog_key())
+        post = db.get(key)
+		
+		#subject = 
+		#post = db.get('select * from PostDB where subject = subject')
+
+        if not post:
+            self.error(404)
+            return
+
+        self.render("permalink.html", post = post)
+
+class NewPost(MainHandler):
+    def get(self):
+        self.render("newpost.html")
+
+    def post(self):
+        subject = self.request.get('subject')
+        content = self.request.get('content')
+
+        if subject and content:
+            p = PostDB(parent = blog_key(), subject = subject, content = content)
+            p.put()
+            self.redirect('/blog')
+        else:
+            error = "subject and content, please!"
+            self.render("newpost.html", subject=subject, content=content, error=error)
+
+app = webapp2.WSGIApplication([('/blog/?', Blog),
+                               ('/blog/post?id=([0-9]+)', Post),
+                               ('/blog/newpost', NewPost),
+                               ],
+                              debug=True)
